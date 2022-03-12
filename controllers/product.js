ea2e61182f008b4a17e053f57278c550d3a25778
huongbtph16610 <buithihuong20080@gmail.com>
2022-03-12 15:39:15 +0700

    list/remove/update

---
+++ controllers/product.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const products = mongoose.model('Product', { name: String });
+
+
+// API thêm sản phẩm
+export const create = async (req, res) => {
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product)
+    } catch (error) {
+        res.status(400).json({
+            message: "Không thêm được sản phẩm "
+        })
+    }
+}
+// API list sản phẩm
+export const list = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
+export const read = (req, res) => {
+    res.json(products.find(item => item.id === +req.params.id));
+}
+export const remove = (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id));
+}
+export const update = (req, res) => {
+    res.json(products.map(item => req.params.id ? req.body : item));
+}
