730dcaf3efa0db9fe1050e74bde9fcff3305849e
huongbtph16610 <buithihuong20080@gmail.com>
2022-03-12 09:33:10 +0700

    buổi 2

--- routers/product.js
+++ routers/product.js
@@ -1,25 +1,24 @@
 import { Router } from 'express';
+import { checkAuth } from '../middlewares/checkAuth';
 const router = Router();
+//fake data
+const products = [
+    { id: 1, name: "Product A" },
+    { id: 2, name: "Product B" }
+];
 
-const check = (req, res, next) => {
-    const status = true;
-    if (status) {
-        next();
-
-    } else {
-        console.log('Không có quyền truy cập');
-    }
-}
-
-router.get('/api/products', check, (req, res) => {
-    console.log(req.body);
-
-    const products = [
-        { id: 1, name: "Product A" },
-        { id: 2, name: "Product B" }
-    ];
+router.get('/products', checkAuth, (req, res) => {
+    res.json(products);
+});
+router.get('/products/:id', checkAuth, (req, res) => {
+    res.json(products.find(item => item.id === +req.params.id));
+});
+router.post('/products', checkAuth, (req, res) => {
     products.push(req.body);
     res.json(products);
+});
+router.get('/products/:id', checkAuth, (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id));
 });
 
 export default router;
--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(productRote);
+app.use("/api", productRote);
 
 //middleware
 //const check = (req, res, next) => {
